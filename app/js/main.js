c11cb79cf7de2802092bf679f57c55b01e7fb4a8
SasaiKudasai12 <vovabro93@gmail.com>
2021-09-02 08:38:40 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $(".star").rateYo({
+        rating: 3.6,
+        starWidth: "17px",
+        normalFill: "#ccccce"
+    });
+});
