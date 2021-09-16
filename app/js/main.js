9513c73f1af80798b745c167c7356bbaada67a00
SasaiKudasai12 <vovabro93@gmail.com>
2021-09-16 03:26:04 +0300

    finish detelis page

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,45 @@ $(function () {
     
   });
   $('.button-list').on('click', function() {
-    $('.product__content-item').addClass('product__content-item--list')
-  })
+    $('.product__content-item').addClass('product__content-item--list');
+    $('.pagination').addClass('pagination--list');
+  });
   $('.button-grid').on('click', function () {
-    $('.product__content-item').removeClass('product__content-item--list')
-  })
+    $('.product__content-item').removeClass('product__content-item--list');
+    $('.pagination').removeClass('pagination--list');
+  });
+
+  $('.detalis-item__input').styler();
+
+  $('.detalis-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.detalis-tabs__top-item').removeClass('detalis-tabs__top-item--active');
+    $(this).addClass('detalis-tabs__top-item--active');
+   
+    $('.detalis-tabs__content-item').removeClass('detalis-tabs__content-item--active')
+    $($(this).attr('href')).addClass('detalis-tabs__content-item--active');
+  });
+
+  $('.detalis-slide__thumb').slick({
+    asNavFor: '.detalis-slide__big',
+    vertical: true,
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: false
+  });
+  $('.detalis-slide__big').slick({
+    asNavFor: '.detalis-slide__thumb',
+    draggable: false,
+    fade: true,
+    arrows: false
+  });
+  $('.detalis-bottomslider__items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  });
+
 
   $(".filter-price__input").ionRangeSlider({
     type: "double",
